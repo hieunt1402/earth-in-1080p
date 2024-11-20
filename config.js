e5c8fbeab6546254c6be89119dd65c189244789b
hieeus <hieunt99hn@gmail.com>
2024-11-20 04:09:52 +0000

    update final

---
+++ config.js
@@ -0,0 +1,7 @@
+const CONFIG = {
+    question:'Nhân ngày nhà giáo Việt Nam, chúc cô giáo luôn vui vẻ, tràn ngập niềm tin trong cuộc sống, ngày nào cũng luôn gặp may mắn, và thành công trên con đường của mình 🥰!! Không có quà gì, mời cô giáo nước canteen nhé 😁',
+    btnReply: 'Order <3',
+    mess: 'Đã nhận thông tin.\n Cảm ơn cô vì những đóng góp cho nền giáo dục Việt Nam 🫡🥰🥳',
+    messDesc: 'Đợi 1 chút để anh confirm đơn hàng nhé!',
+    btnAccept: 'Oki',
+}
